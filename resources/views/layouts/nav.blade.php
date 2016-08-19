a995a501f5ed7254952174fdb22601d48f7f0eb1
Khanh Tran <kmasterycsl@gmail.com>
2016-08-19 15:32:24 +0000

    fix mobile nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,21 +1,26 @@
 <nav class="navbar navbar-inverse">
     <div class="container">
         <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
             <a class="navbar-brand" href="{!! url('/') !!}">
                 <span class="menu-icon fa fa-home"></span>
                 LARMP3
             </a>
         </div>
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+                @include('layouts.partials.li_search')
+                @include('layouts.partials.li_chart')
+                @include('layouts.partials.li_cate')
+            </ul>
 
-        <ul class="nav navbar-nav">
-            @include('layouts.partials.li_search')
-            @include('layouts.partials.li_chart')
-            @include('layouts.partials.li_cate')
-        </ul>
-
-        <ul class="nav navbar-right">
-            @include('layouts.partials.li_auth')
-        </ul>
-
+            <ul class="nav navbar-nav navbar-right">
+                @include('layouts.partials.li_auth')
+            </ul>
+        </div>
     </div>
 </nav>
