249f92e62d01e26d4ef1f071505b25b08ac1217c
Khanh Tran <kmasterycsl@gmail.com>
2016-08-20 02:44:51 +0000

    try minified JS

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -176,7 +176,8 @@ class PlaylistController extends Controller
 
             return view('playlists.show', [
                 'title' => $playlist->playlist_title,
-                'myjs' => ['player.js', 'playlists/show.js'],
+//                'myjs' => ['player.js', 'playlists/show.js'],
+                'myjs' => ['playlists/show.js'],
                 'playlist' => $playlist,
                 'other_playlists' => $other_playlists,
                 'api_url_1' => url("api/get-songs-in-playlist/$playlist->id"),
@@ -186,7 +187,7 @@ class PlaylistController extends Controller
         } else { // Else it is temporary playlist
             $other_playlists = Playlist::inRandomOrder()->take(5)->with('artist','views')->get();
             return view('playlists.guest_show', [
-                'myjs' => ['player.js', 'playlists/guest_show.js'],
+                'myjs' => ['playlists/guest_show.js'],
                 'other_playlists' => $other_playlists,
                 'api_url_1' => url("api/get-songs-in-playlist/danh-sach-tam"),
                 'api_url_2' => url("api/get-nontime-lyrics/")
